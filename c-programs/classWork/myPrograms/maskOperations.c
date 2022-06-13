112436c87ca47f8e3857094010767e683ed54cf7
Christos eleftheriou <itotcozilas@yahoo.com>
2022-06-13 08:39:44 +0300

    before test

--- c-programs/classWork/myPrograms/maskOperations.c
+++ c-programs/classWork/myPrograms/maskOperations.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <inttypes.h>
+void print16b(uint16_t mask){
+    for(int i = 15;i>=0;i--){
+        printf("%d",!!(mask&(1<<i)));
+    }
+    printf("\n");
+}
+
+void mirrorMask(uint16_t* mask){    
+    printf("%d\n",*mask);
+    print16b(*mask);
+    uint16_t mask2 = *mask;
+    for(int i = 7;i>=0;i--){
+    if(!!(*mask&(1<<i))){
+        *mask = *mask|(1<<(i+8));
+    }else *mask = *mask&(~(1<<(i+8)));
+    }
+    print16b(*mask);
+}
 
 int main()
 {
-    uint64_t mask = 2005ul;
+    uint16_t mask = 3654;
 
-    for (int i = 63; i >= 0; i--)
-    {
-        printf("%u", (mask & (1ul << i)) ? 1 : 0);
-    }
-    printf("\n");
+    mirrorMask(&mask);
 
-    mask = mask << 1;
-
-    for (int i = 63; i >= 0; i--)
-    {
-
-        // printf("%u", !!(mask & (1ul << i)));
-    }
-    printf("%lu", mask);
     return 0;
 }
+
+
